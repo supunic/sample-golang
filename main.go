40c239c1b995815a3c3655af1a6305521d9fed32
seiya endo <seiya.endo0830@gmail.com>
2021-01-12 23:18:54 +0900

    条件分岐

--- main.go
+++ main.go
@@ -5,8 +5,15 @@ import (
 )
 
 func main()  {
+	answer := 6
 	var guess int
+
 	fmt.Print("your guess? ")
 	fmt.Scanf("%v", &guess)
-	fmt.Printf("Your guess is %v\n", guess)
+	
+	if answer == guess {
+		fmt.Println("Bingo!")
+	} else {
+		fmt.Println("Boo...")
+	}
 }
