0c6827f435c4e19d239bea661cc8f967ef037091
seiya endo <seiya.endo0830@gmail.com>
2021-01-12 23:31:21 +0900

    カウント処理

--- main.go
+++ main.go
@@ -9,15 +9,17 @@ import (
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	answer := rand.Intn(10) + 1
+	count := 0
 
 	for {
 		var guess int
 
 		fmt.Print("your guess? ")
 		fmt.Scanf("%v", &guess)
+		count++
 
 		if answer == guess {
-			fmt.Println("Bingo!")
+			fmt.Printf("Bingo! It took %v guesses!\n", count)
 			break
 		} else if answer > guess {
 			fmt.Println("The answer is higher!")
